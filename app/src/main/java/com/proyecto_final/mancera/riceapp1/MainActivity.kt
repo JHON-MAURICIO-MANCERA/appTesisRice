5538841e25746cab6eacd2e24f6a9e4f8a88c073
jhon.mancera <dapajhon1502@gmail.com>
2022-01-28 00:09:55 -0500

    se crea activity y se configura el splash screem

--- app/src/main/java/com/proyecto_final/mancera/riceapp1/MainActivity.kt
+++ app/src/main/java/com/proyecto_final/mancera/riceapp1/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.proyecto_final.mancera.riceapp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+        startTimer()
+    }
+
+    fun startTimer(){
+
+        object : CountDownTimer(3000,1000) {
+            override fun onTick(p0: Long) {
+
+            }
+
+            override fun onFinish() {
+              val intent = Intent(applicationContext, PrincipalActivity::class.java).apply {  }
+                startActivity(intent)
+            }
+
+
+        }.start()
     }
 }
---
+++ app/src/main/java/com/proyecto_final/mancera/riceapp1/PrincipalActivity.kt
@@ -0,0 +1,11 @@
+package com.proyecto_final.mancera.riceapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PrincipalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
