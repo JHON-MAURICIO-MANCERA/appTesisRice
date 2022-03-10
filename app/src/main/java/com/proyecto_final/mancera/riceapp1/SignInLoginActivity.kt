2604d6ca1b898d3592872cb38bcdf2d52d7e8e13
Mauricio Mancera <u20132121837@usco.edu.co>
2022-03-10 01:33:39 -0500

    creación de splash inicial

---
+++ app/src/main/java/com/proyecto_final/mancera/riceapp1/SignInLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.proyecto_final.mancera.riceapp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_login)
+    }
+}
